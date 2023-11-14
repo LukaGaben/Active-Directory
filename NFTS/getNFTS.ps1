b2738a4891729d746b84a36c0c40745108136d2c
Luka <lukagabenn@gmail.com>
2023-11-14 20:20:31 +0300

    base

---
+++ NFTS/getNFTS.ps1
@@ -0,0 +1,9 @@
+﻿$corePath = "\\ukkalita.local\iptg\Дивизион управления недвижимостью\DOM"
+$DOM = (Get-Acl $corePath).Access | Select-Object FileSystemRights, AccessControlType, IsInherited, IdentityReference
+$insideDOM = Get-ChildItem $corePath
+
+foreach ($a in $insideDOM) {
+
+    (Get-Acl $a.FullName).Access | Where-Object { $_.IsInherited -eq $false } |ft
+
+}
