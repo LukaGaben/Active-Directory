54aff6b131ae77a5120427619f838e442b295a31
Luka <lukagabenn@gmail.com>
2023-11-21 17:44:17 +0300

    Добавлена проверка типа объектов в группе

---
+++ dhsb.ps1
@@ -0,0 +1,14 @@
+﻿$groupName = "fa RO HR Обучение"
+$groupMembers = Get-ADGroupMember -Identity $groupName
+
+foreach ($member in $groupMembers) {
+    $objectClass = (Get-ADObject -Identity $member.DistinguishedName -Properties objectClass).objectClass
+
+    if ($objectClass -eq "user") {
+        Write-Host "Пользователь: $($member.Name)"
+    } elseif ($objectClass -eq "group") {
+        Write-Host "Группа: $($member.Name)"
+    } else {
+        Write-Host "Неопределенный тип объекта для: $($member.Name)"
+    }
+}
--- getUsersFormGroup.ps1
+++ getUsersFormGroup.ps1
@@ -9,8 +9,18 @@ function Get-UserInfo {
     $users = @() #Переменная куда сохроняем данные 
     $usersGroupName = Get-ADGroupMember -Identity $groupName
     foreach ($userGroupName in $usersGroupName ) {
-        $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, Company
-        $users += $temp
+        $objectClass = (Get-ADObject -Identity $userGroupName.DistinguishedName -Properties objectClass).objectClass
+        if ($objectClass -eq "user") {
+            $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, Company
+            $users += $temp
+        }
+        elseif ($objectClass -eq "group") {
+            Write-Host "Это группа, а не пользователь $($userGroupName.Name)"
+            $users += $userGroupName
+        }
+        else {
+            Write-Host "Неопределенный тип объекта для: $($userGroupName.Name)"
+        }
     }  
     return $users
 }
@@ -23,10 +33,10 @@ if (Get-ADGroup -Filter { Name -eq $groupName }) {
         # Если папки нет, то создаем её
         New-Item -ItemType Directory -Path $folderPath -ErrorAction SilentlyContinue | Out-Null
     }
-    $nameFile = "Входят в  $groupName.csv"
+    $nameFile = "Входят в $groupName.csv"
     $filePath = Join-Path $folderPath $nameFile # Формирование полного пути к файлу
     $usersInfo | Select-Object Name, Company | Export-Csv -Path $filePath -Encoding UTF8 -Delimiter ";" -NoTypeInformation  # Экспорт данных в CSV-файл
-    Write-host "Результат сохранен в папке " $folderPath  
+    Write-host "Результат сохранен в папке " $filePath 
 }
 else {
     Write-Host "Такой группы нет или ошибка в названии группы"
