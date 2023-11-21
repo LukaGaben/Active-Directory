dfbf0961f8b0fdb9ea62f9ac12f818617ec9f864
Luka <lukagabenn@gmail.com>
2023-11-21 14:24:06 +0300

    I fixed the file name formation and changed the way I get the company name

--- getUsersFormGroup.ps1
+++ getUsersFormGroup.ps1
@@ -3,19 +3,18 @@ Param(
 [Parameter (Mandatory=$true, Position=1)]
 [string] $groupName
 ) 
-#Проверка групп. Если ошибка, то вывести сообщение
+#Проверка групп. Если ошибка, то вывести сообщение. Так же чтою избежать прооблем с лишними пробелами, удаляю их
 try {
-    $usersGroupName = Get-ADGroupMember -Identity $groupName #.trim()
+    $usersGroupName = Get-ADGroupMember -Identity $groupName.trim()
 }
 catch {
     Write-Host "Нет такой группы или ошибка в названии"
 }
 $usersInfo = @() #Переменная куда сохроняем данные 
+
 foreach ($userGroupName in $usersGroupName ){
-    $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, DistinguishedName
-    $temp.DistinguishedName = $temp.DistinguishedName -replace ',OU=IPTG,DC=ukkalita,DC=local', '' 
-    $temp.DistinguishedName = ($temp.DistinguishedName -replace 'OU=', '').Split(",",2)[1]
-    $usersInfo +=$temp
+    $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, Company
+    $usersInfo += $temp
 }  
 # Создание папки, где будут лежать результаты
 $folderPath = "C:\results"
@@ -24,9 +23,7 @@ if (-not (Test-Path $folderPath -PathType Container)) {
     # Если папки нет, то создаем её
     New-Item -ItemType Directory -Path $folderPath -ErrorAction SilentlyContinue
 }
+$nameFile = "Входят в  $groupName.csv"
 $filePath = Join-Path $folderPath $nameFile # Формирование полного пути к файлу
-$usersInfo | Select-Object Name, DistinguishedName | Export-Csv -Path $filePath -Encoding UTF8 -Delimiter ";" -NoTypeInformation  # Экспорт данных в CSV-файл
-
- 
-
-
+$usersInfo | Select-Object Name, Company| Export-Csv -Path $filePath -Encoding UTF8 -Delimiter ";" -NoTypeInformation  # Экспорт данных в CSV-файл
+Write-host "Результат сохранен в папке " $folderPath
