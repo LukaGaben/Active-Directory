6343059f4a5ce12037415280ebc9e46f81a1d727
Luka <lukagabenn@gmail.com>
2023-11-21 15:39:56 +0300

    Переписала структуру скрипта и добавила проверку существовании группы в домене

--- getUsersFormGroup.ps1
+++ getUsersFormGroup.ps1
@@ -1,29 +1,33 @@
 ﻿#Ввод имени с консоли 
 Param(
-[Parameter (Mandatory=$true, Position=1)]
-[string] $groupName
+    [Parameter (Mandatory = $true, Position = 1)]
+    [string] $groupName
 ) 
-#Проверка групп. Если ошибка, то вывести сообщение. Так же чтою избежать прооблем с лишними пробелами, удаляю их
-try {
-    $usersGroupName = Get-ADGroupMember -Identity $groupName.trim()
+$groupName = $groupName.Trim()
+function Get-UserInfo {
+    param ()
+    $users = @() #Переменная куда сохроняем данные 
+    $usersGroupName = Get-ADGroupMember -Identity $groupName
+    foreach ($userGroupName in $usersGroupName ) {
+        $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, Company
+        $users += $temp
+    }  
+    return $users
 }
-catch {
-    Write-Host "Нет такой группы или ошибка в названии"
+if (Get-ADGroup -Filter { Name -eq $groupName }) {
+    $usersInfo = Get-UserInfo $groupName
+    # Создание папки, где будут лежать результаты
+    $folderPath = "C:\results"
+    # Проверка существования папки
+    if (-not (Test-Path $folderPath -PathType Container)) {
+        # Если папки нет, то создаем её
+        New-Item -ItemType Directory -Path $folderPath -ErrorAction SilentlyContinue | Out-Null
+    }
+    $nameFile = "Входят в  $groupName.csv"
+    $filePath = Join-Path $folderPath $nameFile # Формирование полного пути к файлу
+    $usersInfo | Select-Object Name, Company | Export-Csv -Path $filePath -Encoding UTF8 -Delimiter ";" -NoTypeInformation  # Экспорт данных в CSV-файл
+    Write-host "Результат сохранен в папке " $folderPath  
 }
-$usersInfo = @() #Переменная куда сохроняем данные 
-
-foreach ($userGroupName in $usersGroupName ){
-    $temp = Get-ADUser -Identity $userGroupName -Properties * | select Name, Company
-    $usersInfo += $temp
-}  
-# Создание папки, где будут лежать результаты
-$folderPath = "C:\results"
-# Проверка существования папки
-if (-not (Test-Path $folderPath -PathType Container)) {
-    # Если папки нет, то создаем её
-    New-Item -ItemType Directory -Path $folderPath -ErrorAction SilentlyContinue
+else {
+    Write-Host "Такой группы нет или ошибка в названии группы"
 }
-$nameFile = "Входят в  $groupName.csv"
-$filePath = Join-Path $folderPath $nameFile # Формирование полного пути к файлу
-$usersInfo | Select-Object Name, Company| Export-Csv -Path $filePath -Encoding UTF8 -Delimiter ";" -NoTypeInformation  # Экспорт данных в CSV-файл
-Write-host "Результат сохранен в папке " $folderPath
