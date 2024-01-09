222b737af4bb38853c49753c2c0a5e3bf800fe44
Luka <lukagabenn@gmail.com>
2024-01-09 14:02:29 +0300

    add new scripts

---
+++ cheakNSTFInheritance.ps1
@@ -0,0 +1,28 @@
+﻿$Pathes = Import-Csv "C:\001\path.csv"
+foreach ($path in $Pathes) {
+    $path = $path.name
+    $corePath = "\\ukkalita.local\iptg\Дивизион управления недвижимостью\" + $Path
+    $folders = Get-childitem $corePath -Directory
+    $folders = $folders.fullname
+    $b = @() 
+    foreach ($folder in $folders) {
+        if ($folder -ne "\\ukkalita.local\iptg\Дивизион управления недвижимостью\DOM") {
+            Write-Host "We are here " $folder
+            $allFolder = Get-childitem $folder -Recurse -Directory 
+            $allFolder = $allFolder.fullName
+            foreach ($bb in  $allFolder) {
+                $a = get-acl -LiteralPath $folder | select Path, AreAccessRulesProtected
+                $b += $a
+                $a = get-acl -LiteralPath $bb | select Path, AreAccessRulesProtected
+                if ($a.AreAccessRulesProtected -eq $true) {
+                    $a.path = $a.Path -replace ".*::", ""
+                    $b += $a
+                }
+            }
+        }
+        #$folderName = $folder -replace "\\ukkalita.local\iptg\Дивизион управления недвижимостью\Департамент эксплуатации объектов недвижимости", ""
+        $folderName += $folder -replace [regex]::Escape("\\ukkalita.local\iptg\Дивизион управления недвижимостью\"), ""
+    }
+    $path 
+    $b | Export-Csv "C:\001\3\$path.csv" -Encoding UTF8 -Delimiter ";" -NoTypeInformation 
+}
--- cheakUsers.ps1
+++ cheakUsers.ps1
@@ -2,17 +2,17 @@ $existUsersName = Import-Csv "C:\001\existUsersName.csv"
 $usersFired = Import-Csv "C:\001\users.csv"
 # Получить данные о пользователях Active Directory
 $users = Get-ADUser -Filter *
-$a=@()
+$a = @()
 # Пройти по каждому уволенному сотруднику
 foreach ($userFired in $usersFired) {
     $matchedUser = $users | Where-Object { $_.Name -eq $userFired.name }
-   # Write-Host "____"
-   if ($matchedUser) {
-    if ($matchedUser.Enabled -eq $true <#-and $existUsersName.Contains($matchedUser#>) {
-        #Write-Host "$($matchedUser.Name) "
-    $a +=$matchedUser
+    # Write-Host "____"
+    if ($matchedUser) {
+        if ($matchedUser.Enabled -eq $true <#-and $existUsersName.Contains($matchedUser#>) {
+            #Write-Host "$($matchedUser.Name) "
+            $a += $matchedUser
         
+        }
+    }
 }
-}
-}
-$a|select Name, UserPrincipalName,SamAccountName, Enabled, DistinguishedName   | Export-Csv "C:\001\notBlocked1.csv" -Encoding UTF8 -Delimiter ";" -NoTypeInformation 
+$a | select Name, UserPrincipalName, SamAccountName, Enabled, DistinguishedName   | Export-Csv "C:\001\notBlocked1.csv" F
