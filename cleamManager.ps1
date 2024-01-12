7bdb73e29a5a83209c765070936dbc2762befa70
Luka <lukagabenn@gmail.com>
2024-01-12 18:22:12 +0300

    Add new small script which cleam Manager

---
+++ cleamManager.ps1
@@ -0,0 +1,6 @@
+﻿$users = get-aduser -Filter {manager -eq "a.tarakanov"} 
+foreach ($user in $users){
+    get-aduser -Identity $user -Properties *|select Name, manager
+    #set-aduser -Identity $user -Manager $null
+    get-aduser -Identity $user -Properties *|select Name, manager
+}
