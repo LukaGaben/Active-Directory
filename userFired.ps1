10d221302bd1b00801b24d1ef9801f11222ae98d
Luka <lukagabenn@gmail.com>
2024-01-22 13:47:28 +0300

    add new script

---
+++ userFired.ps1
@@ -0,0 +1,12 @@
+﻿Param(
+    [Parameter (Mandatory = $true, Position = 1)]
+    [string] $userFullName
+)  
+$date = (get-date).ToString("dd-MM-yyyy HH:mm:ss") 
+$R7OU = "OU=Р7 Групп,OU=Дивизион управления недвижимостью,OU=IPTG,DC=ukkalita,DC=local"
+$userAD = get-aduser -SearchBase $R7OU -Filter { Name -eq $userFullName } -Properties * 
+Set-ADUser -Identity $userAD -Enabled $False -Clear manager -Replace @{extensionAttribute1 = $date } 
+$userAD = $userAD.DistinguishedName
+$adUserPath = "OU=blocked,OU=!Accounts,DC=ukkalita,DC=local"
+Move-ADObject -Identity $userAD -TargetPath $adUserPath
+Write-Host "Пользователь  $userFullName перенесен в blocked"
